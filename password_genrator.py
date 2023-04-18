0300a062205d316891535bd2695cf002535738bb
Malik Junaid <malik.junaid27@gmail.com>
2023-04-19 01:06:01 +0500

    Now can generate random passwords.
    Simple no input just a random password of length 10 that contains letters digits and punctuation marks

---
+++ password_genrator.py
@@ -0,0 +1,21 @@
+import secrets
+import string
+
+def pass_gen():
+
+
+    # Set the length of the password
+    password_length = 10
+
+    # Define the pool of characters to choose from
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+
+    # Generate a random password
+    password = ''.join(secrets.choice(alphabet) for i in range(password_length))
+
+    # Print the password
+    print("Your password is:", password)
+
+    return password
+
+
