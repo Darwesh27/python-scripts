294d954bbb7d25b7bc8d3c89b2bbe15cec873ba7
Malik Junaid <malik.junaid27@gmail.com>
2023-04-19 03:09:10 +0500

    multi difficulty
    Now there are multiple modes of the game. You can either play easy or difficult.

--- guess_number.py
+++ guess_number.py
@@ -1,28 +1,59 @@
 import random
 
 
-def guess_number():
-    """A number guessing game"""
-    secret_number = random.randint(1, 100)
-    num_guesses = 0
+# function to check if guess is close or not
+def is_close(guess, actual):
+    if abs(guess - actual) <= 5:
+        return True
+    else:
+        return False
 
-    while True:
-        try:
-            guess = int(input("Guess a number between 1 and 100: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
 
-        num_guesses += 1
-
-        if guess == secret_number:
-            print(f"Congratulations! You guessed the number in {num_guesses} guesses.")
-            break
-        elif abs(guess - secret_number) <= 5:
-            print("Close! Guess again.")
+# function for easy difficulty level
+def easy_mode(secret_number):
+    print("Guess a number between 1 and 20. You have 6 attempts.")
+    for i in range(1, 7):
+        guess = int(input("Guess #" + str(i) + ": "))
+        if guess < secret_number:
+            print("Too low!")
+        elif guess > secret_number:
+            print("Too high!")
         else:
-            print("Nope. Guess again.")
+            print("You guessed it!")
+            return
+    print("Sorry, you ran out of attempts. The secret number was " + str(secret_number))
 
 
-if __name__ == "__main__":
-    guess_number()
+# function for difficult difficulty level
+def difficult_mode(secret_number):
+    print("Guess a number between 1 and 100.")
+    num_attempts = 0
+    while num_attempts < 30:
+        guess = int(input("Guess: "))
+        if guess == secret_number:
+            print("Congratulations, you guessed the number!")
+            print("Total attempsts: ", num_attempts)
+            return
+        elif is_close(guess, secret_number):
+            print("Close!")
+        else:
+            print("Not close!")
+        num_attempts += 1
+    print("Sorry, you have used all your attempts. The secret number was:", secret_number)
+
+
+def guess_number():
+    # get difficulty level from user
+    print("Select difficulty level:")
+    print("1. Easy")
+    print("2. Difficult")
+    difficulty = int(input("Enter difficulty level (1 or 2): "))
+
+    # generate secret number
+    secret_number = random.randint(1, 20) if difficulty == 1 else random.randint(1, 100)
+
+    # run appropriate game mode
+    if difficulty == 1:
+        easy_mode(secret_number)
+    else:
+        difficult_mode(secret_number)
